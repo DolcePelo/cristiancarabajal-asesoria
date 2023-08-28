e1c36d08cfe941ce6cdd23569c7a032b81be5270
Alejandro Pelozatto <alejandrojpelozatto@gmail.com>
2023-08-28 02:41:50 -0400

    comence a trabajar en el responsive para mobile

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburgerMenu.addEventListener('click', ()=>{
+    mobileMenu.style.display = 'block'
+})
